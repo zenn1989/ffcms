44cd261520d128b6b3e12d0d899c6a0e39184a38
Mihail <zenn1989@gmail.com>
2013-06-17 17:48:03 +0300

    Управление комментариями - бекенд
    Управление комментариями - листинг + массовое удаление.

--- extensions/modules/comments/back.php
+++ extensions/modules/comments/back.php
@@ -5,26 +5,43 @@ class mod_comments_back implements backend
     private $list_count = 10;
     public function load()
     {
-        global $template,$admin,$language,$system,$database,$constant;
+        global $template,$admin,$language,$system,$database,$constant,$user;
         $action_page_title = $admin->getExtName()." : ";
         $work_body = null;
-        if($admin->getAction() == "manage" || $admin->getAction() == null)
+        if($admin->getAction() == "manage" || $admin->getAction() == "list" || $admin->getAction() == null)
         {
+            $notify = null;
+            if($system->post('delete_comments') && $system->post('check_array') != null)
+            {
+                $to_delete_comments = $system->altimplode(',', $system->post('check_array'));
+                if($system->isIntList($to_delete_comments))
+                {
+                    $stmt = $database->con()->prepare("DELETE FROM {$constant->db['prefix']}_mod_comments WHERE id in ($to_delete_comments)");
+                    $stmt->execute();
+                    $notify .= $template->stringNotify('success', $language->get('admin_modules_comment_success_massdel'));
+                }
+            }
             $action_page_title .= $language->get('admin_modules_comment_manage_title');
+            $theme = $template->tplget('comments_list', 'modules/', true);
+            $manage_theme = $template->tplget('comments_list_manage', 'modules/', true);
             $index_start = $admin->getPage();
             $comment_array = array();
             $stmt = $database->con()->prepare("SELECT * FROM {$constant->db['prefix']}_mod_comments ORDER BY id DESC LIMIT ?,?");
             $stmt->bindParam(1, $index_start, PDO::PARAM_INT);
             $stmt->bindParam(2, $this->list_count, PDO::PARAM_INT);
             $stmt->execute();
-            while($result = $stmt->fetch())
+            $fetchAssoc = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $user->listload($system->extractFromMultyArray('author', $fetchAssoc));
+            foreach($fetchAssoc as $result)
             {
-                $comment_array[] = array('<label class="checkbox">'.$result['id'].'<input type="checkbox" name="comment_action[]" value="1"/><label>', $result['author'], $result['comment']);
+                $edit_link = "?object=modules&id={$admin->getID()}&action=edit&page={$result['id']}";
+                $delete_link = "?object=modules&id={$admin->getID()}&action=delete&page={$result['id']}";
+                $manage_links = $template->assign(array('comment_edit', 'comment_delete'), array($edit_link, $delete_link), $manage_theme);
+                $comment_array[] = array('<label class="checkbox">'.$result['id'].'<input type="checkbox" name="check_array[]" class="check_array" value="'.$result['id'].'"/><label>', $user->get('login', $result['author']), $result['comment'], $manage_links);
             }
-            $table = $admin->tplRawTable(array('ID', 'Постер', 'Содержимое'), $comment_array);
-            $work_body = $table;
-
-
+            $table = $admin->tplRawTable(array($language->get('admin_modules_comment_th1'), $language->get('admin_modules_comment_th2'), $language->get('admin_modules_comment_th3'), $language->get('admin_modules_comment_th4')), $comment_array);
+            $pagination_list = $admin->tplRawPagination($this->list_count, $this->getCommentCount(), 'modules');
+            $work_body = $template->assign(array('comment_table', 'ext_pagination_list', 'notify'), array($table, $pagination_list, $notify), $theme);
         }
         elseif($admin->getAction() == "settings")
         {
@@ -52,6 +69,15 @@ class mod_comments_back implements backend
         $menu_link .= $template->assign(array('ext_menu_link', 'ext_menu_text'), array('?object=modules&id='.$admin->getID()."&action=settings", $language->get('admin_modules_comment_settings_title')), $menu_theme);
         $body_form = $template->assign(array('ext_configs', 'ext_menu', 'ext_action_title'), array($work_body, $menu_link, $action_page_title), $template->tplget('config_head', null, true));
         return $body_form;
+    }
+
+    private function getCommentCount()
+    {
+        global $database,$constant;
+        $stmt = $database->con()->prepare("SELECT COUNT(*) FROM {$constant->db['prefix']}_mod_comments");
+        $stmt->execute();
+        $result = $stmt->fetch();
+        return intval($result[0]/$this->list_count);
     }
 
 }
--- templates/admin/modules/comments_list.tpl
+++ templates/admin/modules/comments_list.tpl
@@ -1,1 +1,21 @@
-{$comment_table}
+{$notify}
+<form action="" method="post">
+    {$comment_table}
+    <a id="checkAll">{$lang::admin_modules_comment_select_all}</a><br /><br />
+    <input type="submit" name="delete_comments" class="btn btn-danger" value="{$lang::admin_modules_comment_masss_delete}">
+</form>
+<div class="pagination pagination-centered">
+    <ul>
+        <li class="disabled"><a href="#">{$lang::admin_extension_pagination_word}: </a></li>
+        {$ext_pagination_list}
+    </ul>
+</div>
+<script>
+    $('#checkAll').click(function()
+    {
+        if($('.check_array').is(':checked'))
+            $('.check_array').attr('checked', false);
+        else
+            $('.check_array').attr('checked', true);
+    });
+</script>
