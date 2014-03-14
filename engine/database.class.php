c86ad2edc12bc22c07a56da7b6a189e12b2fda08
Mihail <zenn1989@gmail.com>
2014-03-14 21:02:34 +0200

    Small fixes

--- engine/database.class.php
+++ engine/database.class.php
@@ -16,7 +16,7 @@ class database extends singleton {
                 self::$link = @new \PDO("mysql:host=".property::getInstance()->get('db_host').";dbname=".property::getInstance()->get('db_name')."", property::getInstance()->get('db_user'), property::getInstance()->get('db_pass'), array(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true, \PDO::ATTR_EMULATE_PREPARES => false, \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_PERSISTENT => false));
             } catch(\PDOException $e) {
                 logger::getInstance()->log(logger::LEVEL_ERR, "Database is down! Check configuration and database server uplink! Log: " . $e->getMessage());
-                exit(template::getInstance()->twigRender('database_down.tpl', array('admin_mail' => property::getInstance()->get('mail_from'))));
+                exit(language::getInstance()->get('database_down_desc') . " " . property::getInstance()->get('mail_from'));
             }
             self::$instance = new self();
         }
